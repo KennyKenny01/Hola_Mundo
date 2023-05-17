5e2f7563c3e1a42d3e8e832212ac5419418716fa
KennyF <Kennedymiguel06@gmail.com>
2023-05-17 15:14:17 -0400

    Variables Aritmeticas

---
+++ Operadores Aritmeticos/Variables_aritmeticas.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+'''print(num1 + num2)
+print(num1 * num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 // num2)
+print(num1 % num2)'''
+
+suma = num1 + num2
+resta = num2-num1
+print(suma)
+print(resta)
