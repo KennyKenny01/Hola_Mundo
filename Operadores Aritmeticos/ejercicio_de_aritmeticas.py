2964e4c7aabe707ae4db123f079648cc2dc9a3d8
KennyF <Kennedymiguel06@gmail.com>
2023-05-17 17:10:10 -0400

    Ejercicio 1 de Operaciones aritmetica

---
+++ Operadores Aritmeticos/ejercicio_de_aritmeticas.py
@@ -0,0 +1,11 @@
+'''num1 = 3 
+num2 = 2
+num3 = 5
+print(((num1+num2) / (num2 * num3 ))**2) Ejercicio con declaracions de variables
+
+calculo = ((3+2 )/ (2*5))**2
+print(calculo)'''#Forma con el ejercicio como declaracion de variable imprimiendo el resultado (Reverso)
+
+calculo = pow(((3 + 2) / (5*2)), 2)
+
+print(calculo)
